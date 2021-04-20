32aaf7c6c7baa8766ab5f1bb2cf8ac8c948e11a6
Shinyoung Kim <rolroralra@gmail.com>
2021-04-20 10:26:33 +0900

    Update InsertionSortAlgorithm.java

--- src/behavioral/strategy/impl/InsertionSortAlgorithm.java
+++ src/behavioral/strategy/impl/InsertionSortAlgorithm.java
@@ -4,12 +4,9 @@ import behavioral.strategy.SortAlgorithm;
 
 import java.util.List;
 
-public class InsertionSortAlgorithm<T extends Comparable<T>> implements SortAlgorithm<T>  {
-    public InsertionSortAlgorithm() {
-    }
-
+public class InsertionSortAlgorithm implements SortAlgorithm  {
     @Override
-    public void sort(List<T> original) {
+    public <T extends Comparable<T>> void sort(List<T> original) {
         int size = original.size();
         for (int i = 1; i < size; i++) {
             for (int j = i; j > 0 && original.get(j).compareTo(original.get(j - 1)) < 0; j--) {
