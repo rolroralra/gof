93ef3e93e3c8265ac5db1f62c2fcc1c2b36ccdbc
Shinyoung Kim <rolroralra@gmail.com>
2021-04-20 11:39:39 +0900

    Update ObserverTest.java

--- src/behavioral/observer/test/ObserverTest.java
+++ src/behavioral/observer/test/ObserverTest.java
@@ -1,7 +1,9 @@
 package behavioral.observer.test;
 
 import behavioral.observer.impl.ObserverImpl;
+import behavioral.observer.impl.ObserverImpl2;
 import behavioral.observer.impl.SubjectImpl;
+import behavioral.observer.impl.SubjectImpl2;
 import lombok.Builder;
 import lombok.Data;
 
@@ -27,13 +29,24 @@ public class ObserverTest {
         System.out.println();
 
         Observer observer = new ObserverImpl<Document>();
-        SubjectImpl<Document> data = new SubjectImpl<>(document);
-        data.addObserver(observer);
+        SubjectImpl<Document> subject = new SubjectImpl<>(document);
+        subject.addObserver(observer);
         document.setId(2);
         document.setDocument("changed Test");
 
         System.out.println("[CHANGED DATA]");
-        data.set(document);
+        subject.set(document);
 
+
+        behavioral.observer.Observer observer2 = new ObserverImpl2<Document>();
+        SubjectImpl2<Document> subject2 = new SubjectImpl2<>(document);
+        subject2.addObserver(observer2);
+
+        document.setId(3);
+        document.setDocument("changed Test -- 2");
+
+        System.out.println("[CHANGED DATA]");
+        subject.set(document);
+        subject2.set(document);
     }
 }
