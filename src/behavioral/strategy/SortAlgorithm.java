50c27bbe7530ea14cee8e0e12f42504287d275af
Shinyoung Kim <rolroralra@gmail.com>
2021-04-20 10:25:53 +0900

    Update SortAlgorithm.java

--- src/behavioral/strategy/SortAlgorithm.java
+++ src/behavioral/strategy/SortAlgorithm.java
@@ -2,6 +2,6 @@ package behavioral.strategy;
 
 import java.util.List;
 
-public interface SortAlgorithm<T extends Comparable<T>> {
-    void sort(List<T> original);
+public interface SortAlgorithm {
+    <T extends Comparable<T>> void sort(List<T> original);
 }
