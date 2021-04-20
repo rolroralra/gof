b9953ef69f270f87a7a42bdb2004d58974eb2ea4
Shinyoung Kim <rolroralra@gmail.com>
2021-04-20 18:18:58 +0900

    Update State.java

--- src/behavioral/state/impl/State.java
+++ src/behavioral/state/impl/State.java
@@ -6,28 +6,36 @@ public enum State implements StateInterface {
     STATE01{
         @Override
         public void execute01(Context context) {
-            System.out.println(this + "... execute01()");
+            System.out.println("CURRENT STATE : " + context.getState());
+            System.out.println("... execute01()");
             context.setState(STATE02);
+            context.execute02();
         }
     },
     STATE02 {
         @Override
         public void execute02(Context context) {
-            System.out.println(this + "... execute02()");
+            System.out.println("CURRENT STATE : " + context.getState());
+            System.out.println("... execute02()");
             context.setState(STATE03);
+            context.execute03();
         }
 
         @Override
         public void execute04(Context context) {
-            System.out.println(this + "... execute04()");
+            System.out.println("CURRENT STATE : " + context.getState());
+            System.out.println("... execute04()");
             context.setState(STATE01);
+            System.out.println("Completed!");
         }
     },
     STATE03{
         @Override
         public void execute03(Context context) {
-            System.out.println(this + "... execute03()");
+            System.out.println("CURRENT STATE : " + context.getState());
+            System.out.println("... execute03()");
             context.setState(STATE02);
+            context.execute04();
         }
     };
 
