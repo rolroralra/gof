cbe000bb1835d764f0aa1d72ffe19dd2bb52d30b
Shinyoung Kim <rolroralra@gmail.com>
2021-10-20 16:24:37 +0900

    refactor: setting JUnit test code

--- src/test/shape/ShapeTest.java
+++ src/test/shape/ShapeTest.java
@@ -1,12 +1,14 @@
 package test.shape;
 
+import org.junit.jupiter.api.Test;
 import test.shape.impl.Circle;
 import test.shape.impl.Ellipse;
 import test.shape.impl.Line;
 import test.shape.impl.Rectangle;
 
 public class ShapeTest {
-    public static void main(String[] args) {
+    @Test
+    void test_Shape_Test() {
         Shape circle = new Circle<>(Point.newPoint(1, 2), 1);
         Shape line = new Line<>(Point.newPoint(1,2), Point.newPoint(3, 4));
         Shape rectangle = new Rectangle<>(Point.newPoint(1, 2), Point.newPoint(2, 3), Point.newPoint(3, 4), Point.newPoint(4, 1));
